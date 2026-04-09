c35cc77985f8d28863c2c2cb38e6a89760619c96
TengMengHouy <la0989975@gmail.com>
2026-04-09 11:02:03 +0700

    push build folder

--- src/main/java/com/istad/stadoor/tunnelserver/infrastructure/config/KafkaConfig.java
+++ src/main/java/com/istad/stadoor/tunnelserver/infrastructure/config/KafkaConfig.java
@@ -7,6 +7,7 @@ import org.axonframework.extensions.kafka.KafkaProperties;
 import org.axonframework.extensions.kafka.eventhandling.consumer.ConsumerFactory;
 import org.axonframework.extensions.kafka.eventhandling.consumer.DefaultConsumerFactory;
 import org.axonframework.extensions.kafka.eventhandling.consumer.streamable.StreamableKafkaMessageSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,15 +18,31 @@ import java.util.Map;
 @Configuration
 public class KafkaConfig {
 
+    @Value("${axon.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${KAFKA_PASSWORD}")
+    private String kafkaPassword;
+
     @Bean
-    public ConsumerFactory<String, byte[]> consumerFactory(KafkaProperties kafkaProperties) {
+    public ConsumerFactory<String, byte[]> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "tunnel-projection-group");
         props.put(ConsumerConfig.ISOLATION_LEVEL_CONFIG, "read_committed");
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // ✅ SASL config
+        props.put("security.protocol", "SASL_PLAINTEXT");
+        props.put("sasl.mechanism", "PLAIN");
+        props.put("sasl.jaas.config",
+                "org.apache.kafka.common.security.plain.PlainLoginModule required " +
+                        "username=\"kafka\" " +
+                        "password=\"" + kafkaPassword + "\";"
+        );
+
         return new DefaultConsumerFactory<>(props);
     }
 
