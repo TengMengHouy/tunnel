70db6b7d5c0b9793ac5cc26a383b6f20e36708ed
TengMengHouy <la0989975@gmail.com>
2026-04-11 14:26:36 +0700

    push build folder

--- src/main/java/com/istad/stadoor/tunnelserver/application/dto/request/CreateTunnelRequest.java
+++ src/main/java/com/istad/stadoor/tunnelserver/application/dto/request/CreateTunnelRequest.java
@@ -1,8 +1,12 @@
 package com.istad.stadoor.tunnelserver.application.dto.request;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.UUID;
 
 public record CreateTunnelRequest(
-        @NotBlank UUID userId,
-        @NotBlank String basePath) {}
+        @NotNull(message = "userId is required")
+        UUID userId,
+
+        @NotBlank(message = "basePath is required")
+        String basePath) {}
